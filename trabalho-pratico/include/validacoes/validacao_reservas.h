3efe8616591bb3776be7734382da0775f7f830f9
cliff <a107323@alunos.uminho.pt>
2026-01-08 14:27:10 +0000

    otimiza parsing e reservas sem expor structs

--- trabalho-pratico/include/validacoes/validacao_reservas.h
+++ trabalho-pratico/include/validacoes/validacao_reservas.h
@@ -2,11 +2,17 @@
 #define VALIDACAO_RESERVAS_H
 
 #include <glib.h>
+#include <stddef.h>
 #include "../entidades/reservas.h"
 #include "../gestores/gestor_voos.h"
 #include "../gestores/gestor_passageiros.h"
 
 reserva_t *valida_reserva_from_csv(char **colunas);
+gboolean valida_reserva_campos(char **colunas,
+                               const char **out_flight_ids,
+                               size_t *out_num_voos,
+                               const char **out_document_number,
+                               double *out_preco);
 
 GPtrArray *validar_reserva(const reserva_t *r,
                            gestor_voos_t *gestor_voos,
